a31f4a52989dfe1236c9255d2378f91429bd155e
Edson Jordan <edson_4555@hotmail.com>
2021-06-03 02:18:21 -0500

    añadiendo formulario de agregar post

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -7,18 +7,31 @@
 @section('content')
     <div class="card">
         <div class="card-header">
+
             {!! Form::open(['route' => 'admin.posts.store', 'autocomplete' => 'off']) !!}
+            {!! Form::hidden('user_id', auth()->user()->id) !!}
             <div class="form-group">
                 {!! Form::label('name', 'nombre') !!}
                 {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el nombre del Post']) !!}
+            @error('name')
+                <small class="text-danger" >{{$message}}</small>
+            @enderror
             </div>
             <div class="form-group">
                 {!! Form::label('slug', 'Slug') !!}
                 {!! Form::text('slug', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el slug del Post', 'readonly']) !!}
+                
+            @error('slug')
+                <small class="text-danger" >{{$message}}</small>
+            @enderror
             </div>
             <div class="form-group">
                 {!! Form::label('category_id','Categoria') !!}
                 {!! Form::select('category_id', $categories, null, ['class' => 'form-control']) !!}
+            <hr>
+            @error('category_id')
+                <small class="text-danger" >{{$message}}</small>
+            @enderror
             </div>
             <div class="form-group">
                  
@@ -29,7 +42,11 @@
                         {!! Form::checkbox('techonologies[]', $technology->id, null) !!}
                         {{$technology->name}}
                         </label>
-                    @endforeach                              
+                    @endforeach    
+                <hr>
+            @error('techonologies')
+                <small class="text-danger" >{{$message}}</small>
+            @enderror
             </div>
             <div class="form-group">
                 <p class="font-weight-blod" >Estado</p>
@@ -41,14 +58,27 @@
                     {!! Form::radio('status', 2, null,['class' => 'form-control']) !!}
                     Publicado
                 </label>
+                <hr>
+                @error('status')
+                <small class="text-danger" >{{$message}}</small>
+            @enderror
             </div>
             <div class="form-group">
                 {!! Form::label('extract', 'Extracto:') !!}
                 {!! Form::textarea('extract', null, ['class' => 'form-control']) !!}
+                
+            @error('extract')
+                <small class="text-danger" >{{$message}}</small>
+            @enderror
             </div>
+
             <div class="form-group">
                 {!! Form::label('body', 'Cuerpo del Post:') !!}
                 {!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+            
+            @error('body')
+                <small class="text-danger" >{{$message}}</small>
+            @enderror
             </div>
             {!! Form::submit('Crear Post', ['class' => 'btn btn-success']) !!}
         </div>
